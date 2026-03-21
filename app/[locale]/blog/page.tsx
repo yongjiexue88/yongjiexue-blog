55d4fcb382f94f558c8f4d6e53fc45dad90c205e
yongjiexue88 <yongjiexue88@gmail.com>
2026-03-20 20:28:12 -0500

    feat: migrate to react-portfolio-template (Vite)
    - Replaced Next.js portfolio with Vite React template
    - Preserved balloon parallax images
    - Integrated react-next-parallax inside the main Layout component
    - Added LayoutParallaxBackground.jsx utilizing react-next-parallax

--- app/[locale]/blog/page.tsx
+++
@@ -1,9 +0,0 @@
-// next
-import { redirect } from 'next/navigation';
-
-const Blog = () => {
-  // redirect to the first page
-  redirect('/blog/page/1');
-};
-
-export default Blog;
--- settings/rootUrl.ts
+++
@@ -1,4 +0,0 @@
-export const rootUrl =
-  process.env.NODE_ENV === 'production'
-    ? 'https://yongjiexue.com'
-    : 'http://localhost:3000';
